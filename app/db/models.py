85c95313b3ab9bf9cad10d4c64cc167ed0c6b144
David Enriquez <81264608+David-Enriquez77@users.noreply.github.com>
2025-06-12 09:16:33 -0700

    Add Prediction model with relevant fields for prediction data storage

--- app/db/models.py
+++ app/db/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Float, DateTime
 from app.db.database import Base
 from datetime import datetime
 
@@ -10,3 +10,15 @@ class User(Base):
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
    
+
+class Prediction(Base):
+    __tablename__ = "predictions"
+
+    id = Column(Integer, primary_key=True, index=True)
+    sepal_length = Column(Float)
+    sepal_width = Column(Float)
+    petal_length = Column(Float)
+    petal_width = Column(Float)
+    predicted_class = Column(Integer)
+    predicted_label = Column(String)
+    created_at = Column(DateTime, default=datetime.now)

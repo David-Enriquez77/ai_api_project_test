606ad98b4e608ec7ea968d334869d668661eddbf
David Enriquez <81264608+David-Enriquez77@users.noreply.github.com>
2025-06-12 09:15:55 -0700

    Refactor route imports and add prefixes and tags for clarity

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-
 from fastapi import FastAPI
-from app.auth import register
-from app.auth import login
-from app.api import user_routes
+from app.auth import register, login
+from app.api.routes import user_routes
 from app.db.database import Base, engine
-
+from app.api.routes.predict_routes import router as predict_router
 
 # Create tables if they do not exist
 Base.metadata.create_all(bind=engine)
 
+# Initialize FastAPI application
 app = FastAPI(title="AI API con Registro de Usuario")
 
 # Include routes
-app.include_router(register.router)
-app.include_router(login.router)
-app.include_router(user_routes.router)
+app.include_router(register.router, prefix="/auth", tags=["Auth"])
+app.include_router(login.router, prefix="/auth", tags=["Auth"])
+app.include_router(user_routes.router, prefix="/users", tags=["Users"])
+app.include_router(predict_router, prefix="/predict", tags=["Prediction"])

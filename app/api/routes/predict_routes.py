c1faf1b97c9ab0bd8d1ba1dea21458929c95d6ba
David Enriquez <81264608+David-Enriquez77@users.noreply.github.com>
2025-06-12 13:17:14 -0700

    Refactor prediction endpoint to improve input validation and streamline data processing

--- app/api/routes/predict_routes.py
+++ app/api/routes/predict_routes.py
@@ -1,26 +1,31 @@
+
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.schemas.predict import IrisInput
 from app.db.database import get_db
 from app.db.models import Prediction
+from app.utils.model_loader import session
+import numpy as np
 
-from app.utils.model_loader import session  # <-- importamos la sesión
-
-router = APIRouter()
+router = APIRouter(tags=["Prediction"])
 
 label_map = {0: "Setosa", 1: "Versicolor", 2: "Virginica"}
 
-@router.post("/predict")
+@router.post("/", summary="Make a prediction with the Iris ONNX model")
 def predict(input: IrisInput, db: Session = Depends(get_db)):
-    data = [input.features]
-
     try:
-        inputs = {session.get_inputs()[0].name: data}
-        outputs = session.run(None, inputs)
-        pred_class = int(outputs[0][0])
+        data = np.array([input.features], dtype=np.float32)
+        data = np.ascontiguousarray(data)
+
+        if data.shape != (1, 4):
+            raise HTTPException(status_code=400, detail="Input must have exactly 4 decimal values.")
+
+        model_input = {session.get_inputs()[0].name: data}
+        output = session.run(None, model_input)
+
+        pred_class = int(np.argmax(output[0], axis=1)[0])
         pred_label = label_map.get(pred_class, "Unknown")
 
-        # Guardar en base de datos
         prediction = Prediction(
             sepal_length=data[0][0],
             sepal_width=data[0][1],

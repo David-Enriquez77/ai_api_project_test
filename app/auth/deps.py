f44c977fa2aa29189f3359b5ae6bb32c04db0031
David-Enriquez77 <Dh128555@gmail.com>
2025-06-12 19:27:16 -0600

    Refactor user retrieval logic in authentication to use username instead of user_id

--- app/auth/deps.py
+++ app/auth/deps.py
@@ -5,15 +5,19 @@ from sqlalchemy.orm import Session
 from app.db.database import get_db
 from app.db.models import User
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 #obtaining user from the token
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)) -> User:
-    payload = decode_token(token)
-    user_id: int = payload.get("user_id")
-    if user_id is None:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
-    
-    user = db.query(User).filter(User.id == user_id).first()
-    if user is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    print('token', token)
+    try:
+        payload = decode_token(token)
+        username: str = payload.get("sub")
+        if username is None:
+            raise HTTPException(status_code=401, detail="Invalid token")
+    except Exception:
+        raise HTTPException(status_code=401, detail="Could not validate credentials")
+
+    user = db.query(User).filter(User.username == username).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
     return user

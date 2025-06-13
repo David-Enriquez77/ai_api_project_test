05ed6ab4256eac95df7a1e7145a00534f52df42c
David-Enriquez77 <Dh128555@gmail.com>
2025-06-12 20:05:16 -0600

    Add comment to clarify the purpose of the IrisInput model in prediction schemas

--- app/schemas/predict.py
+++ app/schemas/predict.py
@@ -4,7 +4,7 @@ from datetime import datetime
 # This code defines Pydantic models for input and output schemas related to Iris flower predictions.
 class IrisInput(BaseModel):
     features: List[float] = Field(..., min_items=4, max_items=4)
-    
+#This model represents the input data for making predictions, requiring exactly 4 decimal values representing the features of the Iris flower. 
 class PredictionOut(BaseModel):
     id: int
     sepal_length: float
